9f0afa149864dd4d9e120fb78e489ec09d449ef9
alamarnissi <ala.marnissi30@gmail.com>
2023-03-14 17:48:53 +0100

    some fixes

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -10,12 +10,12 @@ const Footer = () => {
       <LinkList>
         <LinkColumn>
           <LinkTitle>Call</LinkTitle>
-          <LinkItem href="tel:314-343-3432">314-343-3432</LinkItem>
+          <LinkItem href="tel:004-111-2089">004-111-2089</LinkItem>
         </LinkColumn>
         <LinkColumn>
           <LinkTitle>Email</LinkTitle>
-          <LinkItem href="mailto:contact@jsmastery.com">
-            contact@jsmastery.com
+          <LinkItem href="mailto:contact@mycompany.com">
+            contact@mycompany.com
           </LinkItem>
         </LinkColumn>
       </LinkList>
